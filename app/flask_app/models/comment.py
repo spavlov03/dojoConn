60a304fedc09bb36e747cb8f856fb0d60b7a5fbd
00G4 <hakopekizyan3@gmail.com>
2023-01-28 15:22:23 -0800

    forgot to import post model to comment.py

--- app/flask_app/models/comment.py
+++ app/flask_app/models/comment.py
@@ -1,6 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
-from flask_app.models import user
+from flask_app.models import post, user
 
 class Comment:
   DB = "dojo_connect"
