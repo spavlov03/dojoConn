de5a949874d46b090e2dffd7a8622280043edf0d
Maleko Osby <whoskoh123@gmail.com>
2023-06-15 23:49:29 -0700

    navbar pretty much complete, minor adjustments to be made

--- flask_app/static/js/navbar.js
+++ flask_app/static/js/navbar.js
@@ -2,89 +2,48 @@
 
 // dropdown
 let nav = document.querySelector('nav');
-let dropdown = nav.querySelector('.dropdown');
-let dropdownToggle = nav.querySelector("[data-action='dropdown-toggle']");
+let dropdownToggle = nav.querySelector(".dropdown");
+let dropdownMenu = nav.querySelector('.dropdown-menu');
 let navToggle = nav.querySelector("[data-action='nav-toggle']");
 
-// searchbar
-let searchBtn = document.querySelector('.searchBtn');
-let closeBtn = document.querySelector('.closeBtn');
-let searchBox = document.querySelector('.searchBox');
-
-//mobile searchbar 
-let mobilesearchBtn = document.querySelector('.mobilesearchBtn');
-let mobilecloseBtn = document.querySelector('.mobilecloseBtn');
-// let searchBox = document.querySelector('.searchBox');
-
-// usertab & dropdown
-let userBtn = document.querySelector('.userTab li');
-let userTabMenu = document.querySelector(".userTabDropdownMenu");
-
-
-
-// dropdown events
 dropdownToggle.addEventListener('click', () => {
-    if (dropdown.classList.contains('show')) {
-        dropdown.classList.remove('show');
-    } else {
-        dropdown.classList.add('show');
-    }
+    dropdownToggle.classList.toggle('active')
+    dropdownMenu.classList.toggle('active');
 })
 
 navToggle.addEventListener('click', () => {
     if (nav.classList.contains('opened')) {
-        nav.classList.remove('opened');
+        nav.classList.toggle('opened');
     } else {
         nav.classList.add('opened');
     }
 })
 
 
-// search bar events
+
+// searchbar
+let searchBtn = document.querySelector('.searchBtn');
+let closeBtn = document.querySelector('.closeBtn');
+let searchBox = document.querySelector('.searchBox');
+
 searchBtn.addEventListener('click', () => {
-        searchBox.classList.add('active');
-        closeBtn.classList.add('active');
-        searchBtn.classList.add('active');
-    })
+    searchBox.classList.toggle('active');
+    closeBtn.classList.toggle('active');
+    searchBtn.classList.toggle('active');
+})
 
 closeBtn.addEventListener('click', () => {
-    searchBox.classList.remove('active');
-    closeBtn.classList.remove('active');
-    searchBtn.classList.remove('active');
+searchBox.classList.toggle('active');
+closeBtn.classList.toggle('active');
+searchBtn.classList.toggle('active');
 }) 
 
-// mobile search events
-mobilesearchBtn.addEventListener('click', () => {
-    searchBox.classList.add('active');
-    mobilecloseBtn.classList.add('active');
-    mobilesearchBtn.classList.add('active');
-})
 
-mobilecloseBtn.addEventListener('click', () => {
-    searchBox.classList.remove('active');
-    mobilecloseBtn.classList.remove('active');
-    mobilesearchBtn.classList.remove('active');
-}) 
+// usertab & dropdown
+let userBtn = document.querySelector('.userTabBtn');
+let userTabMenu = document.querySelector(".userTabDropdownMenu");
 
-// usertab & dropdown events
 userBtn.addEventListener('click', () => {
-    userTabMenu.classList.add('active')
-    if (window.innerWidth >= 960 && userTabMenu.classList.contains('active') && userTabMenu.style.display === 'block') {
-        userTabMenu.style.display = 'none';
-        userTabMenu.classList.remove('active')
-    }
-    else if(window.innerWidth <= 960 && userTabMenu.classList.contains('active') &&  userTabMenu.style.display === 'flex') {
-        userTabMenu.style.display = 'none';
-        userTabMenu.classList.remove('active')
-    }
-    else if(window.innerWidth <= 960) {
-        userTabMenu.style.display = 'flex';
-    }
-    else {
-        userTabMenu.style.display = 'block';
-    }
+    userTabMenu.classList.toggle('active');
+    userBtn.classList.toggle('active')
 })
-
-// userBtn.addEventListener('click', ()=> {
-//     userTabMenu.classList.remove('active')
-// })
